7382b87e3744f33c780a8ebd329edadc68e3bcd4
DavidHJ2 <132413575+DavidHJ2@users.noreply.github.com>
2025-09-06 19:59:13 -0500

    Updated CheckoutView.tsx

--- src/components/CheckoutView.tsx
+++ src/components/CheckoutView.tsx
@@ -262,6 +262,7 @@ const CheckoutView: React.FC<CheckoutViewProps> = ({
             </button>
             <div className="w-24"></div> {/* Spacer for centering */}
           </div>
+        </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Delivery Information Form */}
           <div className="lg:col-span-2 space-y-6">
